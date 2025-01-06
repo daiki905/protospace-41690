cda01c45ca49247094096589d94cd7fec6f43a71
daiki905 <daikidenanika@yahoo.co.jp>
2025-01-06 19:22:03 +0900

    first commit3

--- db/migrate/20250105090859_create_comments.rb
+++ db/migrate/20250105090859_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
      
-      t.text :content  null: false, default: ""
+      t.text :content,  null: false, default: ""
       t.references :user, foreign_key: true
       t.references :prototype, foreign_key: true
       t.timestamps
